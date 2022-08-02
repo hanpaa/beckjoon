e1ac4604ece72e5bd97dcc7dffb4c9bfc2e984a1
DESKTOP-8MU859D\01064223476 <codehanpa@gmail.com>
2022-08-02 12:01:01 +0900

    arraylist도 첨부터 생성해줘야함 그냥 배열로 수정

--- src/main/java/Beck2606.java
+++ src/main/java/Beck2606.java
@@ -1,5 +1,3 @@
-import sun.awt.image.ImageWatched;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,32 +11,43 @@ public class Beck2606 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] memo = new int[100];
+        int[] memo = new int[101];
+        int[][] graph = new int[101][101];
+        for(int i = 0; i <= 100; i++){
+            memo[i] =0;
+        }
+        for(int i = 0; i <= 100; i++){
+            for(int j = 0; i <= 100; i++){
+                graph[i][j] =0;
+            }
+        }
         ArrayList<ArrayList<Integer>> array = new ArrayList<ArrayList<Integer>>();
         String str;
         String[] tokenList;
+        int node = Integer.parseInt(br.readLine());
+
+
         while((str = br.readLine()) != null){
 
             tokenList = str.split(" ");
-            if(tokenList.length == 2){
-                int num1 = Integer.parseInt(tokenList[0]);
+            int num1 = Integer.parseInt(tokenList[0]);
+            //                ArrayList<Integer> newAL = new ArrayList<Integer>();
+            //                newAL.add(num);
+            //                array.add(num, newAL);
                 int num2 = Integer.parseInt(tokenList[1]);
-                if(array.get(num1) != null){
-                    array.get(num1).add(num2);
-                }else{
-                    ArrayList<Integer> newAL = new ArrayList<Integer>();
-                    newAL.add(num2);
-                    array.add(num1,newAL);
-                }
-            }else{
-                ArrayList<Integer> newAL = new ArrayList<Integer>();
-                int num = Integer.parseInt(tokenList[0]);
-                newAL.add(num);
-                array.add(num, newAL);
-            }
+//                if(array.get(num1) != null){
+//                    array.get(num1).add(num2);
+//                }else{
+//                    ArrayList<Integer> newAL = new ArrayList<Integer>();
+//                    newAL.add(num2);
+//                    array.add(num1,newAL);
+//                }
+                graph[num1][num2] = 1;
+                graph[num2][num1] = 1;
+
         }
 
-        dfs(1, memo);
+        dfs(1, memo, array);
 
         br.close();
 
@@ -48,7 +57,10 @@ public class Beck2606 {
 
         memo[startPoint] =1;
 
-        
+        for(int i =0; i<array.get(startPoint).size(); i++){
+            if(array.get(startPoint)!= null & memo[startPoint] == 0)
+                dfs(array.get(startPoint).get(i), memo, array);
+        }
 
 
     }
