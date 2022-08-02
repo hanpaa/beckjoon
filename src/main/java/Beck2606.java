9e689d64a7270592884cfc50e7b847af85848ca1
DESKTOP-GSJUFBO\work <codehanpa@gmail.com>
2022-08-02 13:10:03 +0900

    2606 정답

--- src/main/java/Beck2606.java
+++ src/main/java/Beck2606.java
@@ -8,33 +8,37 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Beck2606 {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] memo = new int[101];
-        int[][] graph = new int[101][101];
-        for(int i = 0; i <= 100; i++){
+        int node = Integer.parseInt(br.readLine());
+        int edgeNumber = Integer.parseInt(br.readLine());
+
+
+        int[] memo = new int[node+1];
+        int[][] graph = new int[node+1][node+1];
+        for(int i = 0; i <= node; i++){
             memo[i] =0;
         }
-        for(int i = 0; i <= 100; i++){
-            for(int j = 0; i <= 100; i++){
+        for(int i = 0; i <= node; i++){
+            for(int j = 0; i <= node; i++){
                 graph[i][j] =0;
             }
         }
         ArrayList<ArrayList<Integer>> array = new ArrayList<ArrayList<Integer>>();
-        String str;
+        String str = null;
         String[] tokenList;
-        int node = Integer.parseInt(br.readLine());
 
 
-        while((str = br.readLine()) != null){
-
+        for(int i =0; i < edgeNumber; i++){
+            str = br.readLine();
             tokenList = str.split(" ");
             int num1 = Integer.parseInt(tokenList[0]);
             //                ArrayList<Integer> newAL = new ArrayList<Integer>();
             //                newAL.add(num);
             //                array.add(num, newAL);
-                int num2 = Integer.parseInt(tokenList[1]);
+            int num2 = Integer.parseInt(tokenList[1]);
 //                if(array.get(num1) != null){
 //                    array.get(num1).add(num2);
 //                }else{
@@ -47,21 +51,22 @@ public class Beck2606 {
 
         }
 
-        dfs(1, memo, array);
+        int count = 0;
+        count = dfs(1, memo, graph, node, count);
+        System.out.println(count-1);
 
         br.close();
 
     }
 
-    public static void dfs(int startPoint,int[] memo, ArrayList<ArrayList<Integer>> array){
+    public static int dfs(int startPoint,int[] memo, int[][]graph,int node, int count){
 
         memo[startPoint] =1;
-
-        for(int i =0; i<array.get(startPoint).size(); i++){
-            if(array.get(startPoint)!= null & memo[startPoint] == 0)
-                dfs(array.get(startPoint).get(i), memo, array);
+        count++;
+        for(int i =0; i<=node; i++){
+            if(graph[startPoint][i] == 1 && memo[i] == 0)
+                count = dfs(i, memo, graph,node, count);
         }
-
-
+        return count;
     }
 }
